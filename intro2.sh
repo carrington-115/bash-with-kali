4a9d03f634aac8e88db7e2a6cfa4fdcb0d9e5519
Mark <mark@kali>
2024-11-21 23:48:25 -0500

    wrote script to collect the user's name and book

---
+++ intro2.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# getting the user's variables
+echo "What is your name"
+read name
+echo "What is the name of this book?"
+read bookname
+
+# printing the inputed data
+echo "My name is $name and I am reading $bookname"
